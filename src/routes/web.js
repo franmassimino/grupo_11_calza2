0f4dda7cbd6136871f5ac5823ae3cfedb99abfc7
franmassimino <franmassi1904@gmail.com>
2021-06-26 19:59:54 -0300

    home terminado

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,17 @@ const path = require('path')
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../views/index.html'))
 })
-app.get('/product.detail', (req, res) => {
+app.get('/product-detail', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../views/productDetail.html'))
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/login.html'))
+})
+app.get('/product-cart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/productCart.html'))
+})
+
+
+
 module.exports = app
