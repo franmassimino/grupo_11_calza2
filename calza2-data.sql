990012299bc65b4be3b74b544c049fe9259707af
franmassimino <67247715+franmassimino@users.noreply.github.com>
2021-09-29 08:37:40 -0300

    Add files via upload

---
+++ calza2-data.sql
@@ -0,0 +1,26 @@
+INSERT INTO colors VALUES (1,'Negro'), (2,'Blanco'), (3,'Rojo'), (4,'Verde'), (5,'Naranja'), (6,'Violeta'), (7,'Gris');
+
+INSERT INTO brands VALUES (1,'Nike'), (2,'Adidas'), (3,'Puma'), (4,'Fila');
+
+INSERT INTO categories VALUES (1,'Running'), (2,'Urbanas'), (3,'Diseñador');
+
+INSERT INTO users(id,user,email,password,img) VALUES (1,'Francisco','franmassi1904@gmail.com','$2b$10$s/bPioo3qGRzWXv.tYOzwOoXh1l4dSZuQl0tAYYi00M5/NxgFwWBW','/img/uploads/users/default.png');
+
+INSERT INTO products(id,brand,model,price,description,img,category,destacada,color) VALUES
+ (1,1,'Air Force one',520,'Las Zapatillas Nike Air Force 1 están diseñadas e inspiradas en el icónico modelo AF1, su capellada en cuero sintético te da un estilo retro y único de la marca. Su entresuela de espuma te proporciona pisadas más amortiguadas y estables, fáciles de combinar con cualquier tipo de prendas para que luzcas siempre a la moda.','/img/uploads/products/zapatilla1.png',2,1,1)
+,(2,2,'Originals 11',450,'Diseño innovador que combina lo clásico y lo moderno. Presenta una parte superior de malla suave y transpirable, perfecta para tu día a día. El sistema de lazada exclusivo aporta un toque vanguardista a tu estilo.','/img/uploads/products/zapatilla2.png',1,1,5)
+,(3,1,'Lebron 18',930,'Con tecnología Knitposite 2.0 ligera sobre una unidad Zoom Air integral, que ayuda a mantener la velocidad necesaria para superar con facilidad a los defensas y llegar a la canasta.','/img/uploads/products/zapatilla3.png',2,1,4)
+,(4,1,'Air Max 97',1580,'Con el mismo diseño ondulante de las originales inspirado en los trenes bala japoneses, las Nike Air Max 97 te ofrecen un look que potencia tu estilo a toda velocidad. Con la innovadora unidad Nike Air completa que revolucionó el mundo del running y nuevos color y detalles impecables, te permiten disfrutar de la comodidad de primera clase.','/img/uploads/products/zapatilla4.png',3,1,1)
+,(10,4,'Ray',490,'El sneaker Fila Ray presenta una suela exterior audaz y una parte superior de tela mixta para un estilo inmejorable, diseñado con la tendencia de chunky sneakers en mente.','/img/uploads/products/zapatilla10.png',2,1,1)
+,(5,2,'Performance',320,'Con estas zapatillas adidas en tus pies podrás correr en el parque y luego tomar un café con amigos con total comodidad. Su exterior de malla ofrece transpirabilidad que mantiene tus pies frescos de la mañana a la noche. La suela tipo cupsole de caucho brinda la estabilidad necesaria para enfrentarte a todo lo que te depare el día','/img/uploads/products/zapatilla5.png',1,0,1)
+,(6,2,'Ultra Boost 20',720,'Prototipo tras prototipo. Innovación tras innovación. Prueba tras prueba. Uníte a nosotros en la busqueda constante de maximizar y armonizar el peso, la comodidad y en rendimiento. Ultraboost 20. Descubrí un retorno de energía increíble.','/img/uploads/products/zapatilla6.png',1,1,3)
+,(11,3,'Cell Venom',1600,'Cuando la tecnología PUMA CELL apareció por primera vez en 1998, fue revolucionaria. Sus celdas hexagonales ofrecían la forma ideal para una amortiguación estable, por lo que podías moverte de forma más rápida, potente, fluida y enérgica; es decir: mejor. 20 años después, vuelve CELL. Añade un toque diferente a tu estilo urbano con estas zapatillas retro inspiradas en los 80.','/img/uploads/products/zapatilla11.png',3,0,1)
+,(7,1,'Air Max 97 Essential',580,'Las Nike Air Max 97 essential cuentan con el mismo diseño ondulado original, inspirado en los trenes de bala japoneses, para ofrecer un look que impulsa tu estilo a toda velocidad. Con la innovadora unidad Nike Air completa que revolucionó el mundo del running y nuevos color y detalles impecables, te permiten disfrutar de la comodidad de primera clase.','/img/uploads/products/zapatilla7.png',3,0,1)
+,(8,1,'Air More Uptempo',960,'La zapatilla Nike Air More Uptempo, lanzada por primera vez durante la era de alto vuelo del básquetbol de los 90, ha pasado de ser un ícono en la cancha a un ser un calzado urbano de manera casi imperceptible. Con su distintivo diseño de marca, su perfil de confección potente y Nike Air en la planta del pie, su lugar en la historia del calzado es indiscutible.','/img/uploads/products/zapatilla8.png',3,1,1)
+,(9,3,'RS-X 3 Dayzero',1060,'Creado en colaboración con UAL: Central Saint Martin. LLeva un panel superior sintético y de malla, cordones en la parte delantera, lengüeta acolchada con logotipo y detalles de color en contraste. Tirador en la parte posterior, suela de caucho y su composición es de Poliéster.','/img/uploads/products/zapatilla9.png',2,0,1)
+,(12,4,'V94M',700,'FILA V94M un clásico modelo inspirado en los años 90. Su diseño cuenta con muchos detalles, posee una clásica silueta , su entresuela de doble espesor otorga un excelente fit a lo largo del dí­a. Confeccionado en cuero, su entresuela en EVA y su suela en caucho brinda un buen gruip y adherencia en todo tipo de superficies.','/img/uploads/products/zapatilla12.png',3,0,1);
+
+
+
+
+
