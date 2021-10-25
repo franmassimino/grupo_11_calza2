de33c59054054f4ec9f102594ed983071796ce88
franmassimino <67247715+franmassimino@users.noreply.github.com>
2021-10-25 18:57:13 -0300

    Update swiper.js

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -6,7 +6,7 @@ const swiper = new Swiper('.swiper', {
     },
     spaceBetween: 10,
     autoplay: {
-        delay: 4000,
+        delay: 6000,
       },  
     // Navigation arrows
     navigation: {
