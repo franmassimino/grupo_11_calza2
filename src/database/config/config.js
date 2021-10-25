ffd0d62d9a183867cdbf6ba2679e6d54669129bf
franmassimino <franmassi1904@gmail.com>
2021-10-25 14:40:54 -0300

    db produccion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,19 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "bca4ba464d5f19",
+    "password": '00742e3e',
+    "database": "heroku_f96b18d6fa119d2",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bca4ba464d5f19",
+    "password": '00742e3e',
+    "database": "heroku_f96b18d6fa119d2",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
+
+/* mysql://bca4ba464d5f19:00742e3e@us-cdbr-east-04.cleardb.com/heroku_f96b18d6fa119d2?reconnect=true */
